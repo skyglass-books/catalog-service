0da66b7688422e7d555ad6ace6c907705ddb6ffb
skyglass <skyglass2001@gmail.com>
2023-05-13 23:56:28 +0200

    implemented kubernetes config and environment customization with kustomize

--- src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
@@ -5,31 +5,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookService {
 
-    private final BookRepository bookRepository;
+	private final BookRepository bookRepository;
 
-    public BookService(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
 
-    public Iterable<Book> viewBookList() {
-        return bookRepository.findAll();
-    }
+	public Iterable<Book> viewBookList() {
+		return bookRepository.findAll();
+	}
 
-    public Book viewBookDetails(String isbn) {
-        return bookRepository.findByIsbn(isbn)
-                .orElseThrow(() -> new BookNotFoundException(isbn));
-    }
+	public Book viewBookDetails(String isbn) {
+		return bookRepository.findByIsbn(isbn)
+				.orElseThrow(() -> new BookNotFoundException(isbn));
+	}
 
-    public Book addBookToCatalog(Book book) {
-        if (bookRepository.existsByIsbn(book.isbn())) {
-            throw new BookAlreadyExistsException(book.isbn());
-        }
-        return bookRepository.save(book);
-    }
+	public Book addBookToCatalog(Book book) {
+		if (bookRepository.existsByIsbn(book.isbn())) {
+			throw new BookAlreadyExistsException(book.isbn());
+		}
+		return bookRepository.save(book);
+	}
 
-    public void removeBookFromCatalog(String isbn) {
-        bookRepository.deleteByIsbn(isbn);
-    }
+	public void removeBookFromCatalog(String isbn) {
+		bookRepository.deleteByIsbn(isbn);
+	}
 
 	public Book editBookDetails(String isbn, Book book) {
 		return bookRepository.findByIsbn(isbn)
--- src/main/java/com/polarbookshop/catalogservice/web/BookControllerAdvice.java
+++ src/main/java/com/polarbookshop/catalogservice/web/BookControllerAdvice.java
@@ -16,17 +16,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class BookControllerAdvice {
 
-    @ExceptionHandler(BookNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    String bookNotFoundHandler(BookNotFoundException ex) {
-        return ex.getMessage();
-    }
+	@ExceptionHandler(BookNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String bookNotFoundHandler(BookNotFoundException ex) {
+		return ex.getMessage();
+	}
 
-    @ExceptionHandler(BookAlreadyExistsException.class)
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    String bookAlreadyExistsHandler(BookAlreadyExistsException ex) {
-        return ex.getMessage();
-    }
+	@ExceptionHandler(BookAlreadyExistsException.class)
+	@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+	String bookAlreadyExistsHandler(BookAlreadyExistsException ex) {
+		return ex.getMessage();
+	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
