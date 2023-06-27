fad0f859b45b851d2afb8aeb5b6a34116dc8e21a
skyglass <skyglass2001@gmail.com>
2023-06-28 00:56:19 +0200

    added swagger configuration

--- src/main/java/com/polarbookshop/catalogservice/config/SwaggerConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/SwaggerConfig.java
@@ -1,13 +1,14 @@
 package com.polarbookshop.catalogservice.config;
 
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
@@ -35,4 +36,5 @@ public class SwaggerConfig {
     }
 
     public static final String BEARER_KEY_SECURITY_SCHEME = "bearer-key";
+
 }
