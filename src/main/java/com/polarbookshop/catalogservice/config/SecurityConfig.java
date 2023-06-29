69d71cb6d26c12fadea2ce3a4c088f99c2dc009e
skyglass <skyglass2001@gmail.com>
2023-06-29 22:51:41 +0200

    swagger

--- src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/SecurityConfig.java
@@ -18,8 +18,10 @@ public class SecurityConfig {
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
 				.authorizeHttpRequests(authorize -> authorize
-						.mvcMatchers("/actuator/**").permitAll()
+						.mvcMatchers("/books/actuator/**").permitAll()
 						.mvcMatchers(HttpMethod.GET, "/books/**").permitAll()
+						.mvcMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**",
+								"/openapi/**", "/webjars/**").permitAll()
 						.anyRequest().hasRole("employee")
 				)
 				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
